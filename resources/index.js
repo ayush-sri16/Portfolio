246f00f2eb86f6221b8fcebba0a6f6e11478cb75
Ayush Srivastava <114294823+ayush-sri16@users.noreply.github.com>
2023-01-31 18:13:05 +0530

    media queries

--- resources/index.js
+++ resources/index.js
@@ -1,5 +1,3 @@
-AOS.init();
-
 // script for google sheet
 
 var scriptURL = "https://script.google.com/macros/s/AKfycbyQQIG7WO20Z1suwhxB9t2bkoLDg89I5XnjXC6Q45264DGXicZcDjAewj_tFfCn0TC7/exec";
@@ -13,3 +11,17 @@ form.addEventListener('submit', e => {
         .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
         .catch(error => console.error('Error!', error.message))
 });
+
+// Dynamic typing
+var i = -4;
+var txt = " I'm Ayush Srivastava!";
+var speed = 70;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typing").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
